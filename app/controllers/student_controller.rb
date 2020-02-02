fe66678b13e7fe6a5cfacda8efe18811c915be8f
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-02 18:44:36 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,5 +1,9 @@
 class StudentController < ActionController::Base
 
+  # get '/students', to: 'students#index'
+  #   @students = Student.all
+  # end
+  
   def index
     @students = Student.all 
   end 
